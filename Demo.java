7f54b5ce06fefa3a23f136c77227142de849e980
Archita Gupta <archita-gupta@users.noreply.github.com>
2018-10-21 21:09:27 +0530

    Update Demo.java

---
+++ Demo.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+
+
+class Demo
+{
+	public static void main (String[] args)
+	{
+	
+Scanner sc= new Scanner(System.in);
+int n=sc.nextInt();
+if(n==0){
+System.out.println("Zero");
+}
+else if(n<0){
+System.out.println("Negative");}
+else 
+System.out.println("Positive");
+	}
+}
